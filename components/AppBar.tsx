1f35070caa84deb341fdfae34cbd8490ae00f521
Artem <artemskulimovskiy@gmail.com>
2022-10-27 20:42:53 +0100

    basic setup, geolocation feature

---
+++ components/AppBar.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import styles from '../styles/Home.module.css'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+
+export const AppBar: FC = () => {
+    return (
+        <div className={styles.AppHeader}>
+            <span>Solana Notes</span>
+            <WalletMultiButton />
+        </div>
+    )
+}
