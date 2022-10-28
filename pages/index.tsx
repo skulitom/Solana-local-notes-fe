a38d9f6bde8523a51ab26be42d2af2f73da69244
Artem <artemskulimovskiy@gmail.com>
2022-10-28 18:55:59 +0100

    cleaned up geolocation error

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,21 @@ import { Center, Box, Heading } from "@chakra-ui/react";
 import Head from "next/head";
 import { AppBar } from "../components/AppBar"
 import styles from "../styles/Home.module.css";
-import React from "react";
-import { useGeolocated } from "react-geolocated";
+import React, {useEffect, useState} from "react";
 
 export default function Home() {
-  const { coords, isGeolocationAvailable, isGeolocationEnabled } =
-  useGeolocated({
-      positionOptions: {
-          enableHighAccuracy: false,
-      },
-      userDecisionTimeout: 5000,
-  });
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLatitude(position?.coords?.latitude ? position.coords.latitude : 0);
+      setLongitude(position?.coords?.longitude ? position.coords.longitude : 0);
+    });
+  }, [latitude, longitude]);
 
   return (
-    <div className={styles.container} suppressHydrationWarning>
+    <div className={styles.container}>
       <Head>
         <title>Local Notes</title>
         <meta name="description" content="Solana Local Notes" />
@@ -27,12 +28,14 @@ export default function Home() {
           <Heading  className={styles.HeadingText} as="h1" size="1" color="white" ml={4} mt={8}>
             Local Notes
           </Heading>
-          {isGeolocationAvailable ? (
+          {latitude && longitude ? (
           <Heading as="h2" size="l" color="white" ml={4} mt={8}>
             Your location is 
             <Box color="#AA33AA">
-              Lat: {coords?.latitude} <br/>
-              Long: {coords?.longitude}
+              <div suppressHydrationWarning>
+                Lat: {latitude} <br/>
+                Long: {longitude}
+              </div>
             </Box>
           </Heading>) : 
           ( 
